4cf2a7b74437d698cf1bc8b320026fe0e1744eaa
intsuc <intsuc@gmail.com>
2022-03-23 00:07:54 +0900

    Add datapack generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.6.10"
+    application
 }
 
 repositories {
@@ -8,4 +9,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+application {
+    mainClass.set("MainKt")
 }
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,46 @@
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+
+val PACK_0 = """{"pack":{"description":"","pack_format":10}}""".toByteArray()
+val PACK_1 = """{"pack":{"description":"","pack_format":10},"filter":{"block":[{"namespace":"^minecraft$","path":"^(?:advancements|loot_tables|recipes)/"}]}}""".toByteArray()
+val ADVANCEMENT = """{"criteria":{"":{"trigger":"impossible"}}}""".toByteArray()
+val LOOT_TABLE = """{}""".toByteArray()
+val RECIPE = """{"type":"crafting_special_armordye"}""".toByteArray()
+val TAG = """{"replace":true,"values":[]}""".toByteArray()
+
+fun main() {
+    val generated = Paths.get("generated")
+
+    ZipOutputStream(File("VanillaFree0.zip").outputStream().buffered()).use { output ->
+        output.writeFile("pack.mcmeta", PACK_0)
+
+        Files.walk(generated.resolve("data"))
+            .filter { !Files.isDirectory(it) }
+            .map { generated.relativize(it) }
+            .map { path -> path.map { it.toString() } }
+            .forEach { components ->
+                val name = components.joinToString("/")
+                val content = when (val type = components[2]) {
+                    "advancements" -> ADVANCEMENT
+                    "loot_tables" -> LOOT_TABLE
+                    "recipes" -> RECIPE
+                    "tags" -> TAG
+                    else -> throw IllegalStateException("unknown type: '$type'")
+                }
+                output.writeFile(name, content)
+            }
+    }
+
+    ZipOutputStream(File("VanillaFree1.zip").outputStream().buffered()).use { output ->
+        output.writeFile("pack.mcmeta", PACK_1)
+    }
+}
+
+fun ZipOutputStream.writeFile(name: String, content: ByteArray) {
+    putNextEntry(ZipEntry(name))
+    write(content)
+    closeEntry()
+}
